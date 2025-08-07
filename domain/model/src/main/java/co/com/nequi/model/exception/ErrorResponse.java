2f00b12d488f7359920f2d453269073da817dd6c
Milton Sanchez <tomilton@hotmail.com>
2025-08-07 15:10:21 -0500

    add error handling enhancements with custom error response structure and updated test cases

--- domain/model/src/main/java/co/com/nequi/model/exception/ErrorResponse.java
+++ domain/model/src/main/java/co/com/nequi/model/exception/ErrorResponse.java
@@ -1,10 +1,14 @@
 package co.com.nequi.model.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
 
   private String message;
